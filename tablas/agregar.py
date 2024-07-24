9ba9eb08528a6e1631d476f37c84f1b4b4f368e5
mati <matiaslionelgomez11@gmail.com>
2024-07-24 18:37:45 -0300

    Add files via upload

--- tablas/agregar.py
+++ tablas/agregar.py
@@ -1,10 +1,7 @@
 import tkinter as tk
 from ayudas import *
 
-
-
-
-def crear_agregar(root,tree):#6 entrys 
+def crear_agregar(root,tree):#6 entrys y el arbol, es para que al presionar el boton de Ingresar Estudiante obtenga los valores y los inserte(el boton esta al final del codigo)
     def obtener_valores(var_entry1,var_entry2,var_entry3,var_entry4,var_entry5,var_entry6,tree,top):
         valor_entry1 = var_entry1.get()#Curso
         valor_entry2 = var_entry2.get()#Nombre
@@ -25,62 +22,67 @@ def crear_agregar(root,tree):#6 entrys
     var_entry5=tk.StringVar()#DNI
     var_entry6=tk.StringVar()#Observaciones
     
+    
+    #creamos la ventana toplevel para que si se cierra la principal tmb se cierre
     top=tk.Toplevel(root)
     top.title("Agregar un estudiante")
     top.config(bg="#fff")
     top.geometry("460x610")
     
+    #creamos el frame donde va estar el titulo de "Ingresar Estudiante"
     frame_superior=tk.Frame(top,width=460,height=90,bg="#fff")###medidas exactas
     frame_superior.pack(side="top")
     frame_superior.pack_propagate(False)
     
+    #creamos el label del titulo "Ingresar Estudiante"
     titulo=tk.Label(frame_superior,text="Ingresar Estudiante",font=("Times",39,"italic bold"),bg="#fff",fg="#3C5BBA")
     titulo.place(relx=0.5,rely=0.45,anchor="center")
     
     #-----------------------------------------------#
     
+    #creamos el frame que va ocupar el resto de la pantalla (la aprte de abajo del titulo)
     frame_inferior=tk.Frame(top,width=460,height=490,bg="#fff",)###medidas exactas
     frame_inferior.pack(side="top")
     
     #-----------------------------------------------#
 
-    validate_cmd = top.register(solo_numeros)#para dni y F.ingreso, aunq por tema de compatibilidad con la funcino en otro archivo debe ir en cada uno como parametro
+    validate_cmd = top.register(solo_numeros)#para dni y F.ingreso, aunq por tema de compatibilidad con la funcion en otro archivo debe ir en cada uno como parametro
 
     #-----------------------------------------------#
+    #creamos el primer entry
     new_frame=crear_frame_auxiliar(frame_inferior,60)#alto del frame
     #el nuevo frame_aux
     crear_entry_con_img(new_frame,0,0.015,0.0,"Curso",0.065,0.0,var_entry1,0.015,0.5,0.95,validate_cmd)
     #los parametros son: frame donde se guarda,num_img,ubicacion x,y de imagen,texto,ubicacion en x,y del texto la variable en la que se va guardar el textvar y ubicacion en x,y del entry ademas de su largo con el relwidth
     
     #-----------------------------------------------#
-    
+    #segundo entry
     new_frame2=crear_frame_auxiliar(frame_inferior,60)
     crear_entry_con_img(new_frame2,1,0.012,0.0,"Nombre",0.065,0.0,var_entry2,0.015,0.5,0.95,validate_cmd)
     
     #-----------------------------------------------#
-    
+    #tercer entry
     new_frame3=crear_frame_auxiliar(frame_inferior,60)
     crear_entry_con_img(new_frame3,2,0.012,0.0,"Apellido",0.065,0.0,var_entry3,0.015,0.5,0.95,validate_cmd)
     
     #-----------------------------------------------#
-    
+    #cuarto entry
     new_frame4=crear_frame_auxiliar(frame_inferior,60)
     crear_entry_con_img(new_frame4,3,0.016,0.0,"F.Ingreso",0.065,0.0,var_entry4,0.015,0.5,0.95,validate_cmd,only_numbers=True)
     
     #-----------------------------------------------#
-    
+    #quinto entry
     new_frame5=crear_frame_auxiliar(frame_inferior,60)
     crear_entry_con_img(new_frame5,4,0.016,0.0,"DNI",0.065,0.0,var_entry5,0.015,0.5,0.95,validate_cmd,only_numbers=True)
     
     #-----------------------------------------------#
-    
+    #sexto entry
     new_frame6=crear_frame_auxiliar(frame_inferior,150)
     crear_textarea_con_img(new_frame6,5,0.015,0.0,"Observaciones",0.065,0.0,var_entry6,0.015,0.18,0.95,5)
     
     #-----------------------------------------------#
+    #boton de ingresar, que va insertar en la tabla al ser presionado por eso el lambda
     boton_ingresar=tk.Button(top,text="Ingresar Estudiante",bg="#3C5BBA",fg="#fff",font=("Helvetica",18,"bold"),pady=2,bd=2,relief="flat",activebackground="#fff",activeforeground="#3C5BBA", overrelief="solid",command=lambda:obtener_valores(var_entry1,var_entry2,var_entry3,var_entry4,var_entry5,var_entry6,tree,top))
     boton_ingresar.place(relx=0.5,rely=0.93,anchor="center",relwidth=0.97)
-    
-    
     
     top.mainloop()
--- tablas/ayudas.py
+++ tablas/ayudas.py
@@ -1,54 +1,54 @@
 import tkinter as tk
 from PIL import Image,ImageTk
 
-#----------------------------------------------------------------------------#
+#--------------------parte de agregar(por el momento solo agregar)------------------------#
 
+def leer_imagen(ruta,tamaño):
+    return ImageTk.PhotoImage(Image.open(ruta).resize(tamaño,Image.LANCZOS))#hace que funcione la imagen
 
-def crear_entry_con_img(new_frame,i,ubicacionx,ubicaciony,texto,ubi1,ubi2,textvar,ubirelx,ubirely,relwidth,validate_cmd,only_numbers=False):
-    img = imagen(i)#num_img
-    label_img=tk.Label(new_frame,image=img,bg="#fff")
-    label_img.image=img
-    label_img.place(relx=ubicacionx,rely=ubicaciony)#ubicacionx,ubicaciony
-
-    label1=tk.Label(new_frame,text=texto,font=("Times",14),fg="#666a88",bg="#fff")#texto
-    label1.place(relx=ubi1,rely=ubi2)#ubi1,ubi2
-
-    if only_numbers:
-        entry1 = tk.Entry(new_frame, textvariable=textvar, font=("Times", 13), fg="#222", bg="#fff", bd=1, relief=tk.SOLID, validate="key", validatecommand=(validate_cmd, '%S'))
-    else:
-        entry1 = tk.Entry(new_frame, textvariable=textvar, font=("Times", 13), fg="#222", bg="#fff", bd=1, relief=tk.SOLID)
-    entry1.place(relx=ubirelx, rely=ubirely, relwidth=relwidth)
-        
-def imagen(i):
-    camino_imagen=["tablas/imagenes_tabla/Curso.png",
+def imagen(i):#el numero de camino imagen 0,1,2,3,4,5
+    camino_imagen=["tablas/imagenes_tabla/Curso.png",#aca estan todas las imagenes de los entrys 
                    "tablas/imagenes_tabla/nombre.png",
                    "tablas/imagenes_tabla/apellido.png",
                    "tablas/imagenes_tabla/f.ingreso.png",
                    "tablas/imagenes_tabla/dni3.png",
                    "tablas/imagenes_tabla/observacion.png",]
-    img=leer_imagen(camino_imagen[i],(20,20))
+    img=leer_imagen(camino_imagen[i],(20,20)),#uso de la funcion leer imagen y tamaño
     return img
 
-def leer_imagen(ruta,tamaño):
-    return ImageTk.PhotoImage(Image.open(ruta).resize(tamaño,Image.LANCZOS))
+def crear_entry_con_img(new_frame,i,ubicacionx,ubicaciony,texto,ubi1,ubi2,textvar,ubirelx,ubirely,relwidth,validate_cmd,only_numbers=False):
+    img = imagen(i)#num_img
+    label_img=tk.Label(new_frame,image=img,bg="#fff")
+    label_img.image=img#esta linea es inprescindible pq sino pierde la referencia y no anda la imagen
+    label_img.place(relx=ubicacionx,rely=ubicaciony)#ubicacionx,ubicaciony
 
-def crear_frame_auxiliar( master, altura):
+    label1=tk.Label(new_frame,text=texto,font=("Times",14),fg="#666a88",bg="#fff")#texto
+    label1.place(relx=ubi1,rely=ubi2)#ubi1,ubi2
+
+    if only_numbers:#si only numbers es true(es una parametro asi que se puede elegir) hace que solo se pueda usar numeros, lo hace con el validate_cmd(que esta en agregar)
+        entry1 = tk.Entry(new_frame, textvariable=textvar, font=("Times", 13), fg="#222", bg="#fff", bd=1, relief=tk.SOLID, validate="key", validatecommand=(validate_cmd, '%S'))
+    else:
+        entry1 = tk.Entry(new_frame, textvariable=textvar, font=("Times", 13), fg="#222", bg="#fff", bd=1, relief=tk.SOLID)
+    entry1.place(relx=ubirelx, rely=ubirely, relwidth=relwidth)
+        
+#para los inputs crea un frame, para que luego podamos poner la img y el label al lado y el input abajo
+def crear_frame_auxiliar( master, altura):#master es dentro de donde se va ubicar
     frame_auxiliar = tk.Frame(master, bg="#fff", width=460, height=altura)
     frame_auxiliar.pack(side=tk.TOP, fill="x")
     frame_auxiliar.pack_propagate(False)
     return frame_auxiliar
 
 
-    
 def solo_numeros(char):
     return char.isdigit()
 #La función solo_numeros sirve para comprobar si un carácter dado es un dígito. Es útil cuando quieres asegurarte de que sólo se permiten números en un Entry de Tkinter.
 #si es un numero devuelve true sino devuelve false
 
-
+#sirve para obtener lo que se escribio en el textarea(observacion)
 def actualizar_textvar(event, textarea, textvar):
     textvar.set(textarea.get("1.0", tk.END))
 
+#crea el textarea y con parametros le damos la imagen y lo ubicamos, ademas de darle la variable donde se va guardar lo que escriba
 def crear_textarea_con_img(new_frame, i, ubicacionx, ubicaciony, texto, ubi1, ubi2, textvar, ubirelx, ubirely, relwidth, altura):
     img = imagen(i)  # num_img
     label_img = tk.Label(new_frame, image=img, bg="#fff")
@@ -62,6 +62,7 @@ def crear_textarea_con_img(new_frame, i, ubicacionx, ubicaciony, texto, ubi1, ub
     textarea.place(relx=ubirelx, rely=ubirely, relwidth=relwidth)  # ubirelx, ubirely, relwidth
     
     textarea.bind("<KeyRelease>", lambda event: actualizar_textvar(event, textarea, textvar))
+    #no lo entendi del todo pero hace que se pueda rescatar el texto que escriba , ya que en el tk.Text no te deja usar "textvariable="
 
 
-#----------------------------------------------------------------------------#
+#-------------------------------fin de agregar---------------------------------------------#
